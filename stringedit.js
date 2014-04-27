383803f263f99fad5e4bbaf16c3f221347a63c1e
Richelle Cohen <rbcohen@umass.edu>
2014-04-27 13:47:28 -0400

    Added Soundex Algorithm
    Added the Soundex algorithm to help with words that might be spelled
    wildly differently but should be the same because of the way the two
    words sound.

--- stringedit.js
+++ stringedit.js
@@ -54,3 +54,69 @@ function damerauLevenshteinDistance(s, t) {
   // Return the strings' distance
   return d[m][n];
 }
+
+
+//Soundex Algorithm
+
+var toDelete = new Array("a", "e", "i", "o", "u", "y", "h", "w");
+var labial = new Array("b", "f", "p", "v");
+var weirdSounds = new Array("c", "g", "j", "k", "q", "s", "x", "z");
+var alveolarStops = new Array("t", "d");
+var liquids = new Array("l");
+var nasals = new Array("m", "n");
+var rhotics = new Array("r");
+
+function calculateVal(a){		//used to change spelling to sound values
+	a = a.split("");
+	var nA = "";
+	var j;
+	var i;
+	nA += a[0].toUpperCase();
+	for(i=1; i<a.length; i++){
+		if($.inArray(a[i], labial) !== -1){		//inArray finds the index and returns -1 if it doesn't exist
+			nA += 1;
+		}
+		else if ($.inArray(a[i], weirdSounds) !==-1){
+			nA += 2;
+		}
+		else if ($.inArray(a[i], alveolarStops) !== -1){
+			nA += 3
+		}
+		else if ($.inArray(a[i], liquids) !== -1){
+			nA += 4
+		}
+		
+		else if ($.inArray(a[i], nasals) !== -1){
+			nA += 5
+		}
+		
+		else if ($.inArray(a[i], rhotics) !== -1){
+			nA += 6
+		}
+		else{  //anything not in the preceding categories can be ignored
+			continue;
+		}
+		
+	}
+	return nA;
+}
+
+function getSoundex(a, b){
+	//calculate the Soundex distance between two strings
+	var soundA = calculateVal(a);
+	var soundB = calculateVal(b);
+	
+	if ( soundA.length !== soundB.length){
+		if(soundB.length < soundA.length){
+			//soundB = addZeros(soundB, soundA);
+			soundA = soundA.substr(0, soundB.length-1);
+		}
+		else{
+			//soundA = addZeros(soundA, soundB);
+			soundB = soundB.substr(0, soundA.length-1);
+			
+		}
+	}
+	return (damerauLevenshteinDistance(soundA, soundB));
+}
+
