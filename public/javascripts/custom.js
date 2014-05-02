fb8f198d2fed8dae81f421656d51e202edc8b6c0
kolovsam <samantha@pointed.com>
2014-05-02 15:10:13 -0400

    back to top floating button

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -10,4 +10,21 @@ $(document).ready(function (){
 			scrollTop: $("#cloud").offset().top
 		}, 1500);
 	}); // close submit button click
+
+    var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(duration);
+        } else {
+            $('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+
 }); // close document ready
