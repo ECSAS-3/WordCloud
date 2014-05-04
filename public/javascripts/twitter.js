5e1e7d0210e0aff6cbdcb90be3eb1cc87fe25e8a
Christopher Scott <ScottDChris@GMail.com>
2014-05-04 01:29:30 -0400

    Refactored twitter script to include initialization in tokenizer function

--- public/javascripts/twitter.js
+++ public/javascripts/twitter.js
@@ -3,44 +3,19 @@
 
 //Christopher Scott
 
-//Twitter Setup
-var util = require('util'),
-twitter = require('twitter');
-var twit = new twitter({
-	consumer_key: '5fBdLw6wzZL9S5AKbfaVfZnGn',
-	consumer_secret: 'SGyNotse02dMUP8C6ehYJ1zGmj1vV95TV53N3cMvGopEUdHFm5',
-	access_token_key: '2437602979-m8kAsy58Sezf5UJ7T18BUndgcyTKhu9gZCJYrda',
-	access_token_secret: '3mHJ98q7stLjSUvY1Wp8aBIa5mqSkpkQZCINXwLqo3Si3'
-});
-
-//Pull search term from command line
-function userQuery(){
-	//Take input from the command line
-	var readline = require('readline');
-
-	var rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	});
-
-	rl.question("What would you like to search for? ", function(term) {
-		twit.search(term, function(data) {
-			console.log(data);
-		});
-		console.log("Querying WordCloud for", term);
-		rl.close();
-	});
-}
-
-function printTweets(term){
-	twit.search(term, function(data) {
-		for (i=0; i<data.statuses.length; i++){
-			console.log(data.statuses[i].text);
-		}
-	});
-}
 var globalArray = {};
+
 function tokenizer(term, callback){
+	//Twitter Setup
+	var util = require('util'),
+	twitter = require('twitter');
+	var twit = new twitter({
+		consumer_key: '5fBdLw6wzZL9S5AKbfaVfZnGn',
+		consumer_secret: 'SGyNotse02dMUP8C6ehYJ1zGmj1vV95TV53N3cMvGopEUdHFm5',
+		access_token_key: '2437602979-m8kAsy58Sezf5UJ7T18BUndgcyTKhu9gZCJYrda',
+		access_token_secret: '3mHJ98q7stLjSUvY1Wp8aBIa5mqSkpkQZCINXwLqo3Si3'
+	});
+
 	twit.search(term, function(data) {
 		var wordArray = {};
 		var len = data.statuses.length;
@@ -67,7 +42,6 @@ function tokenizer(term, callback){
 }
 
 tokenizer("doge", function(w){
-	//console.log(w);
 	for (word in w){
 		if(w[word]>0){
 			console.log("%s - %d", word, w[word]*2);
@@ -76,11 +50,37 @@ tokenizer("doge", function(w){
 });
 
 //Stop Word Functions and Such
-
 var stopWord = new Array("I","a","about","an","are","as","at","be","by","for","from","how","in","is","it","of","on","or","that","that's",
-						"thats","the","this","to","too","was","what","what's","whats","when","when's","whens","where","where's","wheres",
-						"who","who's","whos","whose","will","with","the");
+	"thats","the","this","to","too","was","what","what's","whats","when","when's","whens","where","where's","wheres",
+	"who","who's","whos","whose","will","with","the");
 
 function isStopWord(word){
 	return stopWord.indexOf(word) >-1;
+}
+
+//Pull search term from command line
+function userQuery(){
+	//Take input from the command line
+	var readline = require('readline');
+
+	var rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+	rl.question("What would you like to search for? ", function(term) {
+		twit.search(term, function(data) {
+			console.log(data);
+		});
+		console.log("Querying WordCloud for", term);
+		rl.close();
+	});
+}
+
+function printTweets(term){
+	twit.search(term, function(data) {
+		for (i=0; i<data.statuses.length; i++){
+			console.log(data.statuses[i].text);
+		}
+	});
 }
